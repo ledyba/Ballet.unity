2cd7da130fc40d5f2adf8ac1e4824c76e181ca94
Kaede Fujisaki <kaede@hexe.net>
2021-09-07 20:41:02 +0900

    Generatorを分離した

--- Assets/Behaviours/Generator/EnemyGenerator.cs
+++ Assets/Behaviours/Generator/EnemyGenerator.cs
@@ -5,19 +5,11 @@ namespace Behaviours.Generator
   public class EnemyGenerator : MonoBehaviour
   {
     public GameObject stage;
-    public GameObject black;
-    public GameObject white;
-    public void Generate(int pos)
+    public GameObject enemyPrefab;
+    public void Generate()
     {
-      var x = -1.4f * pos;
-      if (pos % 2 == 0)
-      {
-        Instantiate(white, new Vector3(x, 3), Quaternion.identity, stage.transform);
-      }
-      else
-      {
-        Instantiate(black, new Vector3(x, 4), Quaternion.identity, stage.transform);
-      }
+      Instantiate(enemyPrefab, transform.localPosition, Quaternion.identity, stage.transform);
     }
   }
 }
+
